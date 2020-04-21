6d40b9df2ca130177ecbe429179b344005ed30bd
organized-trainer-0561 <jared.epps322@gmail.com>
2020-04-21 23:33:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,12 +25,12 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.first
+  array
   
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array.first 
 end
 
 def retrieve_last_element_from_array(array)
